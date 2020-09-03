193a98fd3dbdf2d3d53922aa178f08c9a0742bc8
arun54321 <lunar.arun@gmail.com>
2020-09-03 08:56:11 +0530

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,9 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Arun blog`,
+    author: `Arun`,
+    description: `test`,
     social: [
       {
         name: `twitter`,
